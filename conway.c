a55f03aa4a78c25102d0e4f920525a0740c38d81
Moritz Fink <fink.moritz@gmail.com>
2024-02-02 23:36:47 +0100

    Add documentation

--- conway.c
+++ conway.c
@@ -2,28 +2,26 @@
 #include <stdio.h>
 #include <time.h>
  
-
+/* Holds all information needed for the Game of Life. */
 typedef struct conway_game {
     int width;
     int height;
     char* cells;
 } conway_game;
 
-void print(conway_game* game) {
-    for(int h = 0; h < game->height; ++h) {
-        for(int w = 0; w < game->width; ++w) {
-            printf("%d ", game->cells[h * game->width + w] & 0x01);
-        }
-        printf("\n");
-    }
+/* Free memory allocated by this object. */
+void destroy(conway_game* game) {
+    free(game->cells);
+    free(game);
 }
 
+/* Randomly initialize a Game of Life. */
 conway_game* initialize(int width, int height) {
     srand(time(NULL));
 
     char* cells = malloc(sizeof(char) * width * height);
     for(int c = 0; c < width * height; ++c) {
-        cells[c] = (rand() % 2 == 0) ? 0x00 : 0x01;
+        cells[c] = rand() % 2;
     }
 
     conway_game* game = malloc(sizeof(conway_game));
@@ -34,9 +32,14 @@ conway_game* initialize(int width, int height) {
     return game;
 }
 
-void destroy(conway_game* game) {
-    free(game->cells);
-    free(game);
+/* Print the cell map for this Game of Life. */
+void print(conway_game* game) {
+    for(int h = 0; h < game->height; ++h) {
+        for(int w = 0; w < game->width; ++w) {
+            printf("%d ", game->cells[h * game->width + w] & 0x01);
+        }
+        printf("\n");
+    }
 }
 
 int main()
