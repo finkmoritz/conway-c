c7764053679115a3520ec97c7f747536a2e85e49
Moritz Fink <fink.moritz@gmail.com>
2024-02-03 16:45:49 +0100

    Add basic cell map functions

--- include/conway.h
+++ include/conway.h
@@ -7,10 +7,17 @@
  
 /* Holds all information needed for the Game of Life. */
 typedef struct conway_game {
-    int width;
-    int height;
+    unsigned int width;
+    unsigned int height;
     char* cells;
 } conway_game;
+
+/* Returns cell state (1=on or 0=off). */
+int cell_state(conway_game* game, int x, int y);
+
+/* Turns an on-cell off, decrementing the on-neighbor count for the
+   eight neighboring cells. */
+void clear_cell(conway_game* game, unsigned int x, unsigned int y);
 
 /* Free memory allocated by this object. */
 void destroy(conway_game* game);
@@ -20,5 +27,9 @@ conway_game* initialize(int width, int height);
 
 /* Print the cell map for this Game of Life. */
 void print(conway_game* game);
+
+/* Turns an off-cell on, incrementing the on-neighbor count for the
+   eight neighboring cells. */
+void set_cell(conway_game* game, unsigned int x, unsigned int y);
 
 #endif // CONWAY_H_
--- src/conway.c
+++ src/conway.c
@@ -4,6 +4,49 @@
 
 #include "conway.h"
 
+int cell_state(conway_game* game, int x, int y)
+{
+    unsigned char *cell_ptr;
+
+    cell_ptr = game->cells + (y * game->width) + x;
+    return *cell_ptr & 0x01;
+}
+
+void clear_cell(conway_game* game, unsigned int x, unsigned int y)
+{
+    int xoleft, xoright, yoabove, yobelow;
+    unsigned char *cell_ptr = game->cells + (y * game->width) + x;
+
+    // Calculate the offsets to the eight neighboring cells,
+    // accounting for wrapping around at the edges of the cell map
+    if (x == 0)
+        xoleft = game->width - 1;
+    else
+        xoleft = -1;
+    if (y == 0)
+        yoabove = game->width * game->height - game->width;
+    else
+        yoabove = -game->width;
+    if (x == (game->width - 1))
+        xoright = -(game->width - 1);
+    else
+        xoright = 1;
+    if (y == (game->height - 1))
+        yobelow = -(game->width * game->height - game->width);
+    else
+        yobelow = game->width;
+
+    *(cell_ptr) &= ~0x01;
+    *(cell_ptr + yoabove + xoleft) -= 2;
+    *(cell_ptr + yoabove ) -= 2;
+    *(cell_ptr + yoabove + xoright) -= 2;
+    *(cell_ptr + xoleft) -= 2;
+    *(cell_ptr + xoright) -= 2;
+    *(cell_ptr + yobelow + xoleft) -= 2;
+    *(cell_ptr + yobelow) -= 2;
+    *(cell_ptr + yobelow + xoright) -= 2;
+}
+
 void destroy(conway_game* game) {
     free(game->cells);
     free(game);
@@ -32,4 +75,40 @@ void print(conway_game* game) {
         }
         printf("\n");
     }
+}
+
+
+void set_cell(conway_game* game, unsigned int x, unsigned int y)
+{
+    int xoleft, xoright, yoabove, yobelow;
+    unsigned char *cell_ptr = game->cells + (y * game->width) + x;
+
+    // Calculate the offsets to the eight neighboring cells,
+    // accounting for wrapping around at the edges of the cell map
+    if (x == 0)
+        xoleft = game->width - 1;
+    else
+        xoleft = -1;
+    if (y == 0)
+        yoabove = game->width * game->height - game->width;
+    else
+        yoabove = -game->width;
+    if (x == (game->width - 1))
+        xoright = -(game->width - 1);
+    else
+        xoright = 1;
+    if (y == (game->height - 1))
+        yobelow = -(game->width * game->height - game->width);
+    else
+        yobelow = game->width;
+
+    *(cell_ptr) |= 0x01;
+    *(cell_ptr + yoabove + xoleft) += 2;
+    *(cell_ptr + yoabove) += 2;
+    *(cell_ptr + yoabove + xoright) += 2;
+    *(cell_ptr + xoleft) += 2;
+    *(cell_ptr + xoright) += 2;
+    *(cell_ptr + yobelow + xoleft) += 2;
+    *(cell_ptr + yobelow) += 2;
+    *(cell_ptr + yobelow + xoright) += 2;
 }
